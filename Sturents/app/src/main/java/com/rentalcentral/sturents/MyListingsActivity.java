196bf5e5920abee88a7f39b4e13e6ea7c7cbcc4c
Jake <jakejazokas@outlook.com>
2020-02-29 14:50:36 -0500

    Added activity for displaying saved user listings

---
+++ Sturents/app/src/main/java/com/rentalcentral/sturents/MyListingsActivity.java
@@ -0,0 +1,13 @@
+package com.rentalcentral.sturents;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MyListingsActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+}
