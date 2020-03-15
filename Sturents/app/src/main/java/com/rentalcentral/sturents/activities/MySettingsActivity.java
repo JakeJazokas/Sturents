562eb92f7b501e33ec85239c2053f2cf7f178335
PineappleIspro <jefferychen888@gmail.com>
2020-03-15 17:56:43 -0400

    basic sliders implemented for location and Price

--- Sturents/app/src/main/java/com/rentalcentral/sturents/activities/MySettingsActivity.java
+++ Sturents/app/src/main/java/com/rentalcentral/sturents/activities/MySettingsActivity.java
@@ -5,6 +5,9 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageButton;
+import android.widget.ProgressBar;
+import android.widget.SeekBar;
+import android.widget.TextView;
 
 import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
@@ -13,6 +16,12 @@ import com.rentalcentral.sturents.R;
 
 public class MySettingsActivity extends AppCompatActivity {
 
+    private TextView locationView;
+    private ProgressBar locationProgressBar;
+    private SeekBar locationSeekBar;
+    private TextView priceView;
+    private ProgressBar priceProgressBar;
+    private SeekBar priceSeekBar;
     private Button location;
 
     @Override
@@ -41,6 +50,53 @@ public class MySettingsActivity extends AppCompatActivity {
             @Override
             public void onClick(View v) {
                 startActivity(new Intent(MySettingsActivity.this,Pop.class));
+            }
+        });
+
+        //creating location slider
+        locationView = (TextView) findViewById(R.id.locationView);
+        locationProgressBar = (ProgressBar) findViewById(R.id.locationProgressBar);
+        locationSeekBar = (SeekBar) findViewById(R.id.locationSeekBar);
+
+        locationSeekBar.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
+            @Override
+            public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
+                locationProgressBar.setProgress(progress);
+                locationView.setText("" + progress + "km");
+            }
+
+            @Override
+            public void onStartTrackingTouch(SeekBar seekBar) {
+
+            }
+
+            @Override
+            public void onStopTrackingTouch(SeekBar seekBar) {
+
+            }
+        });
+
+        //creating price slider
+
+        priceView = (TextView) findViewById(R.id.priceView);
+        priceProgressBar = (ProgressBar) findViewById(R.id.priceProgressBar);
+        priceSeekBar = (SeekBar) findViewById(R.id.priceSeekBar);
+
+        priceSeekBar.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
+            @Override
+            public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
+                priceProgressBar.setProgress(progress);
+                priceView.setText("$" + progress );
+            }
+
+            @Override
+            public void onStartTrackingTouch(SeekBar seekBar) {
+
+            }
+
+            @Override
+            public void onStopTrackingTouch(SeekBar seekBar) {
+
             }
         });
     }}
